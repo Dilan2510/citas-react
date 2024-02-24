1a6c93344d3fc59da212e4991c293a9e0ad1a4a7
Dilan2510 <brandondilangg@gmail.com>
2024-02-24 00:32:58 -0500

    new changes in the project from the onSubmit

--- src/components/ListPatients.jsx
+++ src/components/ListPatients.jsx
@@ -1,7 +1,15 @@
-function ListPatients() {
+import Patients from "./Patients";
+
+function ListPatients(patients) {
+  console.log();
   return (
-    <div className="w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-scroll">
       <h3 className="font-black text-3xl text-center">Lista Pacientes</h3>
+      <p className="text-lg mt-5 mb-10 text-center font-bold">
+        Administra tus{""}{" "}
+        <span className="text-blue-700 font-bold">Pacientes y Citas</span>
+      </p>
+      <Patients {...patients}></Patients>
     </div>
   );
 }
